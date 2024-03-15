84f48bfdb561a85236444b7e664a38d654454751
mini crabik <agl.malcv@gmail.com>
2024-03-15 16:10:43 +0300

    second commit

--- libs/data_structures/matrix/matrix.c
+++ libs/data_structures/matrix/matrix.c
@@ -2,17 +2,27 @@
 #include <malloc.h>
 #include "matrix.h"
 
-matrix getMemMatrix(int nRows, int nCols) {
-    int **values = (int **) malloc(sizeof(int*) * nRows);
-    for (int i = 0; i < nRows; i++)
-        values[i] = (int *) malloc(sizeof(int) * nCols);
-    return (matrix){values, nRows, nCols};
+matrix getMemMatrix(int n_rows, int n_cols) {
+    int **values = (int **) malloc(sizeof(int*) * n_rows);
+    for (int i = 0; i < n_rows; i++)
+        values[i] = (int *) malloc(sizeof(int) * n_cols);
+    return (matrix){values, n_rows, n_cols};
 }
 
 matrix *getMemArrayOfMatrices(int nMatrices,
-                              int nRows, int nCols) {
+                              int n_rows, int n_cols) {
     matrix *ms = (matrix*) malloc(sizeof(matrix) * nMatrices);
     for (int i = 0; i < nMatrices; i++)
-        ms[i] = getMemMatrix(nRows, nCols);
+        ms[i] = getMemMatrix(n_rows, n_cols);
     return ms;
+}
+
+void free_mem_matrix(matrix *m) {
+    for (int i = 0; i < m->n_rows; i++)
+        free(m->values[i]);
+
+    free(m->values);
+    m->values = NULL;
+    m->n_rows = 0;
+    m->n_cols = 0;
 }
--- libs/data_structures/matrix/matrix.h
+++ libs/data_structures/matrix/matrix.h
@@ -4,22 +4,24 @@
 
 typedef struct matrix {
     int **values; // элементы матрицы
-    int nRows; // количество рядов
-    int nCols; // количество столбцов
+    int n_rows; // количество рядов
+    int n_cols; // количество столбцов
 } matrix;
 
 typedef struct position {
-    int rowIndex;
-    int colIndex;
+    int row_index;
+    int col_index;
 } position;
 
 //размещает в динамической памяти матрицу размером nRows на nCols.
 // Возвращает матрицу.
-matrix getMemMatrix(int nRows, int nCols);
+matrix getMemMatrix(int n_rows, int n_cols);
 
 // размещает в динамической памяти массив из nMatrices матриц размером nRows на nCols.
 // Возвращает указатель на нулевую матрицу
-matrix *getMemArrayOfMatrices(int nMatrices, int nRows, int nCols);
+matrix *getMemArrayOfMatrices(int n_matrices, int n_rows, int n_cols);
 
+// освобождает память выделенную под массив m
+void free_mem_matrix(matrix *m);
 
 #endif //UNTITLED15_MATRIX_H
